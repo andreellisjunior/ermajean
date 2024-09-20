9500c8d13e7f4598f31a17a249aa083f251a0c21
Andre Ellis Jr <andre.ellis@precisionplanting.com>
2024-09-20 14:40:48 -0500

    making more

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Logo from './assets/logo.svg';
+import Logo from '../app/assets/Logo.svg';
 import { SubmitButton } from '@/components/submit-button';
 import { signInAction } from './actions';
 import Link from 'next/link';
