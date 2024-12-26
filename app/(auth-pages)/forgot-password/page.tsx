b3f99674c8335f017feffd7edc044ab1af85cca2
Andre Ellis Jr <andre.ellis@precisionplanting.com>
2024-12-26 12:14:55 -0600

    updating reset

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -1,11 +1,12 @@
-import { forgotPasswordAction } from '@/app/actions';
-import { FormMessage, Message } from '@/components/ui/form-message';
-import { SubmitButton } from '@/components/ui/submit-button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import Link from 'next/link';
-import Logo from '../../assets/Logo.svg';
-import Image from 'next/image';
+import { forgotPasswordAction } from "@/app/actions";
+import { FormMessage, Message } from "@/components/ui/form-message";
+import { SubmitButton } from "@/components/ui/submit-button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import Logo from "../../assets/Logo.svg";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function ForgotPassword({
   searchParams,
@@ -14,34 +15,43 @@ export default function ForgotPassword({
 }) {
   return (
     <>
-      <div className='flex flex-col justify-evenly items-center w-full h-screen p-2'>
+      <div className="flex flex-col justify-evenly items-center w-full h-screen p-2">
         {/* Hero/Logo */}
-        <div className='flex flex-col items-center justify-center text-center'>
+        <div className="flex flex-col items-center justify-center text-center">
           <h3>welcome to</h3>
-          <Image src={Logo} alt='logo' width={500} height={500} />
+          <Image src={Logo} alt="logo" width={500} height={500} />
           <p>Your personal recipe management and creation tool.</p>
         </div>
         {/* Sign up/Sign in Section */}
-        <div className='w-full flex flex-col'>
-          <form className='flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64'>
-            <div>
-              <h1 className='text-2xl font-medium'>Reset Password</h1>
-              <p className='text-sm text-secondary-foreground text-right'>
-                Already have an account?{' '}
-                <Link className='text-primary underline' href='/'>
-                  Sign in
-                </Link>
-              </p>
-            </div>
-            <div className='flex flex-col gap-2 [&>input]:mb-3 mt-8'>
-              <Label htmlFor='email'>Email</Label>
-              <Input name='email' placeholder='you@example.com' required />
-              <SubmitButton formAction={forgotPasswordAction}>
-                Reset Password
-              </SubmitButton>
+        <div className="w-full flex flex-col">
+          {"success" in searchParams ? (
+            <div className="text-foreground flex flex-col gap-4">
               <FormMessage message={searchParams} />
+              <Link href="/sign-in">
+                <Button className="w-full">Sign In</Button>
+              </Link>
             </div>
-          </form>
+          ) : (
+            <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64">
+              <div>
+                <h1 className="text-2xl font-medium">Reset Password</h1>
+                <p className="text-sm text-secondary-foreground text-right">
+                  Already have an account?{" "}
+                  <Link className="text-primary underline" href="/">
+                    Sign in
+                  </Link>
+                </p>
+              </div>
+              <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+                <Label htmlFor="email">Email</Label>
+                <Input name="email" placeholder="you@example.com" required />
+                <SubmitButton formAction={forgotPasswordAction}>
+                  Reset Password
+                </SubmitButton>
+                <FormMessage message={searchParams} />
+              </div>
+            </form>
+          )}
         </div>
       </div>
     </>
--- app/recipes/reset-password/page.tsx
+++ app/recipes/reset-password/page.tsx
@@ -1,12 +1,12 @@
-import { resetPasswordAction } from '@/app/actions';
-import { FormMessage, Message } from '@/components/ui/form-message';
-import { SubmitButton } from '@/components/ui/submit-button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import Image from 'next/image';
-import Logo from '../../assets/Logo.svg';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
+import { resetPasswordAction } from "@/app/actions";
+import { FormMessage, Message } from "@/components/ui/form-message";
+import { SubmitButton } from "@/components/ui/submit-button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
+import Logo from "../../assets/Logo.svg";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function ResetPassword({
   searchParams,
@@ -14,39 +14,39 @@ export default async function ResetPassword({
   searchParams: Message;
 }) {
   return (
-    <div className='flex flex-col justify-evenly items-center w-full h-screen p-2'>
+    <div className="flex flex-col justify-evenly items-center w-full h-screen p-2">
       {/* Hero/Logo */}
-      <div className='flex flex-col items-center justify-center text-center'>
+      <div className="flex flex-col items-center justify-center text-center">
         <h3>welcome to</h3>
-        <Image src={Logo} alt='logo' width={500} height={500} />
+        <Image src={Logo} alt="logo" width={500} height={500} />
         <p>Your personal recipe management and creation tool.</p>
       </div>
       {/* Sign up/Sign in Section */}
-      <div className='w-full flex flex-col'>
-        {'success' in searchParams ? (
-          <div className='text-foreground border-l-2 border-foreground px-4'>
-            <Link href='/'>
-              <Button className='w-full'>Login</Button>
+      <div className="w-full flex flex-col">
+        {"success" in searchParams ? (
+          <div className="text-foreground border-l-2 border-foreground px-4">
+            <Link href="/sign-in">
+              <Button className="w-full">Sign In</Button>
             </Link>
           </div>
         ) : (
-          <form className='flex flex-col w-full min-w-64 p-4 gap-2 [&>input]:mb-4'>
-            <h1 className='text-2xl font-medium'>Reset password</h1>
-            <p className='text-sm text-foreground/60'>
+          <form className="flex flex-col w-full min-w-64 p-4 gap-2 [&>input]:mb-4">
+            <h1 className="text-2xl font-medium">Reset password</h1>
+            <p className="text-sm text-foreground/60">
               Please enter your new password below.
             </p>
-            <Label htmlFor='password'>New password</Label>
+            <Label htmlFor="password">New password</Label>
             <Input
-              type='password'
-              name='password'
-              placeholder='New password'
+              type="password"
+              name="password"
+              placeholder="New password"
               required
             />
-            <Label htmlFor='confirmPassword'>Confirm password</Label>
+            <Label htmlFor="confirmPassword">Confirm password</Label>
             <Input
-              type='password'
-              name='confirmPassword'
-              placeholder='Confirm password'
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm password"
               required
             />
             <SubmitButton formAction={resetPasswordAction}>
