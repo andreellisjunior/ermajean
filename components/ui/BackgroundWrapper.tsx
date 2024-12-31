9f8aeca102c4d98f8ee4e8015ed1a58c82b1ce89
Andre Ellis Jr <andre.ellis@precisionplanting.com>
2024-12-31 07:40:01 -0600

    tweak to innerWidth

--- components/ui/BackgroundWrapper.tsx
+++ components/ui/BackgroundWrapper.tsx
@@ -2,17 +2,32 @@
 import { usePathname } from "next/navigation";
 import bg from "../../app/assets/background.png";
 import RecipeBg from "../../app/assets/RecipesBackground.png";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 const BackgroundWrapper = ({ children }: { children: ReactNode }) => {
   const pathname = usePathname();
   const background = pathname.includes("/recipes") ? RecipeBg : bg;
 
+  const [isWideScreen, setIsWideScreen] = useState(false);
+
+  useEffect(() => {
+    const checkScreenWidth = () => setIsWideScreen(window.innerWidth > 767);
+
+    // Run on mount
+    checkScreenWidth();
+
+    // Add event listener for resize
+    window.addEventListener("resize", checkScreenWidth);
+
+    // Cleanup event listener on unmount
+    return () => window.removeEventListener("resize", checkScreenWidth);
+  }, []);
+
   return (
     <div
       className="bg-background w-full"
       style={
-        window.innerWidth > 767
+        isWideScreen
           ? {
               backgroundImage: `url(${background.src})`,
               position: "fixed",
